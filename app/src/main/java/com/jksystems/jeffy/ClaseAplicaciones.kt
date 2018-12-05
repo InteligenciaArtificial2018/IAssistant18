32fd71450ddf49bc98bbc7f6c6c36552b964c13c
Jeffy6917 <32935443+Jeffy6917@users.noreply.github.com>
2018-12-04 19:41:29 -0600

    Eso no funciona

--- app/src/main/java/com/jksystems/jeffy/ClaseAplicaciones.kt
+++ app/src/main/java/com/jksystems/jeffy/ClaseAplicaciones.kt
@@ -10,12 +10,6 @@ import android.content.ComponentName
 
 class ClaseAplicaciones
 {
-    fun Llamada(tel : String): Unit {
-        val numero = Uri.parse(tel)
-        val intentllamada = Intent(Intent.ACTION_DIAL, numero)
-        startActivity(Main(), intentllamada, Bundle())
-    }
-
     fun Whatsapp(): Unit {
         val contexto = Main()
         val wpp = Intent("android.intent.action.MAIN")
